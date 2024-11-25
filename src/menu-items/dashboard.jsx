c95ab43031be76b3e345fbd89feb449375100121
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-11-26 01:18:01 +0800

    hide for develop pages

--- src/menu-items/dashboard.jsx
+++ src/menu-items/dashboard.jsx
@@ -11,30 +11,30 @@ const dashboard = {
   title: 'Navigation',
   type: 'group',
   children: [
-    {
-      id: 'account',
-      title: 'Account',
-      type: 'item',
-      url: '/app/account',
-      icon: UserOutlined  ,
-      breadcrumbs: false
-    },
-    {
-      id: 'dashboard',
-      title: 'Dashboard',
-      type: 'item',
-      url: '/app/dashboard',
-      icon: DashboardOutlined,
-      breadcrumbs: false
-    },
-    {
-      id: 'inventory',
-      title: 'Inventory',
-      type: 'item',
-      url: '/app/inventory',
-      icon: DropboxOutlined ,
-      breadcrumbs: false
-    },
+    // {
+    //   id: 'account',
+    //   title: 'Account',
+    //   type: 'item',
+    //   url: '/app/account',
+    //   icon: UserOutlined  ,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: 'dashboard',
+    //   title: 'Dashboard',
+    //   type: 'item',
+    //   url: '/app/dashboard',
+    //   icon: DashboardOutlined,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: 'inventory',
+    //   title: 'Inventory',
+    //   type: 'item',
+    //   url: '/app/inventory',
+    //   icon: DropboxOutlined ,
+    //   breadcrumbs: false
+    // },
     {
       id: 'product',
       title: 'Product',
@@ -43,22 +43,22 @@ const dashboard = {
       icon: ProductOutlined  ,
       breadcrumbs: false
     },
-    {
-      id: 'sales',
-      title: 'Sales',
-      type: 'item',
-      url: '/app/sales',
-      icon: ShoppingCartOutlined ,
-      breadcrumbs: false
-    },
-    {
-      id: 'supplier',
-      title: 'Supplier',
-      type: 'item',
-      url: '/app/supplier',
-      icon: AreaChartOutlined ,
-      breadcrumbs: false
-    },
+    // {
+    //   id: 'sales',
+    //   title: 'Sales',
+    //   type: 'item',
+    //   url: '/app/sales',
+    //   icon: ShoppingCartOutlined ,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: 'supplier',
+    //   title: 'Supplier',
+    //   type: 'item',
+    //   url: '/app/supplier',
+    //   icon: AreaChartOutlined ,
+    //   breadcrumbs: false
+    // },
  
    
 
