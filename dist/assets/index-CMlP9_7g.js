2e6e034957ffce7011a705d2aaa517c8bbae8c9c
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-12-01 17:11:38 +0800

    update production

--- dist/assets/index-CMlP9_7g.js
+++
@@ -1,1 +0,0 @@
-import{j as t,M as e,O as r}from"./index-BLu414Qf.js";const a=()=>t.jsx(e,{title:"Supplier",children:t.jsx(r,{})});export{a as default};
---
+++ dist/assets/index-CPLJ0AiL.js
@@ -0,0 +1,1 @@
+import{j as t,M as r,O as s}from"./index-CmW0R-vW.js";const a=()=>t.jsx(r,{title:"Product",children:t.jsx(s,{})});export{a as default};
