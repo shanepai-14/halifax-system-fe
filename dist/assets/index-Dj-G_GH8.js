924849053dd5c5572e33e205052ce79467f9238a
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-12-05 23:49:41 +0800

    update supplier and purchase

---
+++ dist/assets/index-Dj-G_GH8.js
@@ -0,0 +1,1 @@
+import{j as t,M as r,O as s}from"./index-D1JMdfBH.js";const a=()=>t.jsx(r,{title:"Product",children:t.jsx(s,{})});export{a as default};
---
+++ dist/assets/index-sS2XAdpP.js
@@ -0,0 +1,1 @@
+import{j as r,M as t,O as e}from"./index-D1JMdfBH.js";const a=()=>r.jsx(t,{title:"Purchase Order",children:r.jsx(e,{})});export{a as default};
