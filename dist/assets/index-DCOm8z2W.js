823b3cb371933b5263d251a30977ac5e183914c4
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-01-14 23:43:12 +0800

    update po page

---
+++ dist/assets/index-DCOm8z2W.js
@@ -0,0 +1,1 @@
+import{j as t,M as r,O as s}from"./index-DSrAcRxp.js";const a=()=>t.jsx(r,{title:"Product",children:t.jsx(s,{})});export{a as default};
---
+++ dist/assets/index-DGy_qvHu.js
@@ -0,0 +1,1 @@
+import{j as r,M as t,O as e}from"./index-DSrAcRxp.js";const a=()=>r.jsx(t,{title:"Purchase Order",children:r.jsx(e,{})});export{a as default};
