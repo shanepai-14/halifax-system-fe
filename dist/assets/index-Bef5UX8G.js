61c519ee2e117df25342a94c41313089f40e682d
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-02-28 05:59:27 +0800

    update

--- dist/assets/index-Bef5UX8G.js
+++
@@ -1,1 +0,0 @@
-import{j as t,M as e,O as r}from"./index-CRPfqlGd.js";const a=()=>t.jsx(e,{title:"Supplier",children:t.jsx(r,{})});export{a as default};
---
+++ dist/assets/index-Sxlj4Tk2.js
@@ -0,0 +1,1 @@
+import{j as r,M as t,O as e}from"./index-Cbx92YL5.js";const a=()=>r.jsx(t,{title:"Purchase Order",children:r.jsx(e,{})});export{a as default};
