6ef47366db6e87d2b90a622da89e86de949a3a71
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-03-13 09:23:18 +0800

    update

---
+++ dist/assets/index-C97Lgrve.js
@@ -0,0 +1,1 @@
+import{j as e,M as t,O as s}from"./index-BH3gsM3a.js";const i=()=>e.jsx(t,{title:"Receiving Reports",children:e.jsx(s,{})});export{i as default};
--- dist/assets/index-DmRch3bF.js
+++
@@ -1,1 +0,0 @@
-import{j as t,M as e,O as n}from"./index-B4lmkI_6.js";const a=()=>t.jsx(e,{title:"Inventory Management",children:t.jsx(n,{})});export{a as default};
