d034bb33fd7446bd07ea26b6cde24f2779f594d4
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-04-02 16:39:20 +0800

    update

--- dist/assets/index-7QXfL0wB.js
+++
@@ -1,1 +0,0 @@
-import{j as s,M as t,O as e}from"./index-jDIXAb2S.js";const a=()=>s.jsx(t,{title:"Customers",children:s.jsx(e,{})});export{a as default};
---
+++ dist/assets/index-azU9CYRV.js
@@ -0,0 +1,1 @@
+import{j as e,M as s,O as t}from"./index-CrSDUfpv.js";const a=()=>e.jsx(s,{children:e.jsx(t,{})});export{a as default};
