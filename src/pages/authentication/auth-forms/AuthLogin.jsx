f757715e939a766effc7344a37608aa9f7a94384
Shane <96838519+shanepai-14@users.noreply.github.com>
2025-06-21 14:54:22 +0800

    update

--- src/pages/authentication/auth-forms/AuthLogin.jsx
+++ src/pages/authentication/auth-forms/AuthLogin.jsx
@@ -147,9 +147,9 @@ export default function AuthLogin({ isDemo = false }) {
                     }
                     label={<Typography variant="h6">Keep me sign in</Typography>}
                   />
-                  <Link variant="h6" component={RouterLink} to="/forgot-password" color="text.primary">
+                  {/* <Link variant="h6" component={RouterLink} to="/forgot-password" color="text.primary">
                     Forgot Password?
-                  </Link>
+                  </Link> */}
                 </Stack>
               </Grid>
               {errors.submit && (
